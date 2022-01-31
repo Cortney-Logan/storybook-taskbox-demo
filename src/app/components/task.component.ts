02688277df16f1aea95fb875e09ccf7202545873
Cortney-Logan <clogan@predictiveindex.com>
2022-01-31 11:02:53 -0500

    build out task template

--- src/app/components/task.component.ts
+++ src/app/components/task.component.ts
@@ -1,19 +1,31 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Task } from '../models/task.model';
 
-@Component({selector: 'app-task',
-template: `
-<div class = "list-item">
-  <input type = "text" [value]="task.title" readonly = 'true'/>
-</div>`})
-export class TaskComponent{
-@Input() task: any;
+@Component({ selector: 'app-task', templateUrl: './task.component.html' })
+export class TaskComponent {
+  @Input() task: Task;
 
-// tslint:disable-next-line: no-output-on-prefix
-@Output()
-onPinTask = new EventEmitter<Event>();
+  // tslint:disable-next-line: no-output-on-prefix
+  @Output()
+  onPinTask = new EventEmitter<Event>();
 
-// tslint:disable-next-line: no-output-on-prefix
-@Output()
-onArchiveTask = new EventEmitter<Event>();
+  // tslint:disable-next-line: no-output-on-prefix
+  @Output()
+  onArchiveTask = new EventEmitter<Event>();
 
+  /**
+   * Component method to trigger the onPin event
+   * @param id string
+   */
+  onPin(id: any): void {
+    this.onPinTask.emit(id);
+  }
+
+  /**
+   * Component method to trigger the onArchive event
+   * @param id string
+   */
+  onArchive(id: any): void {
+    this.onArchiveTask.emit(id);
+  }
 }
