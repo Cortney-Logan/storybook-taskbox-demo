d018c035c56d230537f5f81dfa18d8a2d3e12624
Cortney-Logan <clogan@predictiveindex.com>
2022-02-01 09:19:31 -0500

    add pure inbox screen components

--- src/app/components/pure-inbox-screen.stories.ts
+++ src/app/components/pure-inbox-screen.stories.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgxsModule, Store } from '@ngxs/store';
 import { Meta, moduleMetadata, Story } from '@storybook/angular';
+import { fireEvent, within } from '@testing-library/angular';
 import { TasksState } from '../state/task.state';
 import { PureInboxScreenComponent } from './pure-inbox-screen.component';
 import { TaskModule } from './task.module';
@@ -9,15 +10,14 @@ export default {
   component: PureInboxScreenComponent,
   decorators: [
     moduleMetadata({
-      declarations: [PureInboxScreenComponent],
       imports: [CommonModule, TaskModule, NgxsModule.forRoot([TasksState])],
-      providers: [Store]
+      providers: [Store],
     }),
   ],
   title: 'PureInboxScreen',
 } as Meta;
 
-const Template: Story = args => ({
+const Template: Story = (args) => ({
   props: args,
 });
 
@@ -26,4 +26,13 @@ export const Default = Template.bind({});
 export const Error = Template.bind({});
 Error.args = {
   error: true,
+};
+
+export const WithInteractions = Template.bind({});
+WithInteractions.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+  // Simulates pinning the first task
+  await fireEvent.click(canvas.getByLabelText('pinTask-1'));
+  // Simulates pinning the third task
+  await fireEvent.click(canvas.getByLabelText('pinTask-3'));
 };
